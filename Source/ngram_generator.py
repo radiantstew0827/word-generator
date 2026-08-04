9cf495bd112b4695d852e37bd8d4e60254328590
radiantstew0827 <m3.ladra@gmail.com>
2026-08-04 16:07:10 +0100

    process weights

--- Source/ngram_generator.py
+++ Source/ngram_generator.py
@@ -1,19 +1,42 @@
 
 
-nSize = 4
+nSize = 3
 wordlist_path = "Source/wordlists/maori_wordlist.txt"
 
-def GenerateNGrams(n : int, tokens : str):
-    contextSize = n-1# the n'th character is the one beign predicted. Thfre contextsize is n-1
-    ngrams = []
+def GenerateNGrams(n : int, word : str, ngrams : list[tuple[str, str]]):
+    # add start characters so previous words aren't being read and converted into ngrams
+    # add end character so the generation can choose to end the word
+    contextSize = n - 1 # the n'th character is the one beign predicted. Thfre contextsize is n-1
 
-    for i in range(0, len(tokens) - n):
-        context = tokens[i: i+contextSize] # [inclusive : exclusive]
-        target = tokens[i + contextSize]
+    word = "_" * contextSize + word + "0"
+
+    for i in range(0, len(word) - contextSize):
+        context = word[i:i+contextSize] # [inclusive : exclusive]
+        target = word[i + contextSize]
         ngram = (context, target)
         ngrams.append(ngram)
 
+def ProcessWeights(ngrams : list[tuple[str, str]]) -> dict[str, dict[str, int]]:
+    # outcomesWeights: for each context, is a weighted dictionary for each possible outcome
+    outcomeWeights : dict[str, dict[str, int]] = {}
 
+    for ngram in ngrams:
+        context = ngram[0]
+        target = ngram[1]
+
+        if (context in outcomeWeights):
+            # if context exist, change the probabiblity (weight)
+            if target in outcomeWeights[context]:
+                outcomeWeights[context][target] += 1
+            else:
+                outcomeWeights[context][target] = 1
+
+        else:
+            outcomeWeights[context] = {target : 1}
+            
+
+    print(outcomeWeights)
+    return outcomeWeights
 
 def Main():
     try:
@@ -23,22 +46,13 @@ def Main():
         input()
         return # exit program
 
-    wordlist = file.read()
+    wordlist = file.read().split(" ")
+    ngrams : list[tuple[str, str]] = []
 
-    # add start characters so previous words aren't being read and converted into ngrams
-    contextSize = nSize-1
-    start = "_"*contextSize
+    for word in wordlist:
+        GenerateNGrams(nSize, word, ngrams)
 
-    # add end character so the generation can choose to end the word
-    end = "0"
-    interword = end + start
-
-    # add end and start characters
-    wordlist = wordlist.replace(" ", interword)
-    wordlist = start + wordlist
-
-    # generate ngrams
-    GenerateNGrams(nSize, wordlist)
+    ProcessWeights(ngrams)
 
 if __name__ == "__main__":
     Main()
